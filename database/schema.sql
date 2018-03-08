684158c318b02eef92df99e4dfb69a89e955f8c3
ktr0731 <aizu.s1230022@gmail.com>
2018-03-08 22:44:22 +0900

    add DB schema

---
+++ database/schema.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS gpay;
+CREATE DATABASE gpay;
+
+USE gpay;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+  id VARCHAR(255) PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  display_name VARCHAR(255) not null,
+  amount BIGINT not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS invoices;
+CREATE TABLE invoices(
+  id VARCHAR(255) PRIMARY KEY,
+  status TINYINT not null,
+  from_id VARCHAR(255) not null,
+  to_id VARCHAR(255) not null,
+  amount BIGINT not null,
+  message VARCHAR(255),
+  FOREIGN KEY(from_id) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  FOREIGN KEY(to_id) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS transactions;
+CREATE TABLE transactions(
+  id VARCHAR(255) PRIMARY KEY,
+  tranasction_type TINYINT not null,
+  from_id VARCHAR(255) not null,
+  to_id VARCHAR(255) not null,
+  amount BIGINT not null,
+  message VARCHAR(255),
+  FOREIGN KEY(from_id) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  FOREIGN KEY(to_id) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+SET FOREIGN_KEY_CHECKS=1;
