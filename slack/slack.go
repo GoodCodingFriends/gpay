46e4da81f791294aa69c15617a02b63bc560217d
ktr0731 <ktr@syfm.me>
2019-07-15 18:27:01 +0900

    add app_mention handler

--- slack/slack.go
+++ slack/slack.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
 type commonRequest struct {
@@ -12,6 +15,13 @@ type commonRequest struct {
 
 	// url_verification
 	Challenge string `json:"challenge"`
+
+	// app_mention
+	User           string      `json:"user"`
+	Text           string      `json:"text"`
+	Timestamp      json.Number `json:"ts"`
+	Channel        string      `json:"channel"`
+	EventTimestamp json.Number `json:"event_ts"`
 }
 
 func (r *commonRequest) urlVerificationRequest() *urlVerificationRequest {
@@ -20,17 +30,35 @@ func (r *commonRequest) urlVerificationRequest() *urlVerificationRequest {
 	}
 }
 
+func (r *commonRequest) appMentionRequest() *appMentionRequest {
+	return &appMentionRequest{
+		user:           r.User,
+		text:           r.Text,
+		timestamp:      r.Timestamp,
+		channel:        r.Channel,
+		eventTimestamp: r.EventTimestamp,
+	}
+}
+
 type urlVerificationRequest struct {
 	challenge string
 }
 
-func Handler() http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+type appMentionRequest struct {
+	user           string
+	text           string
+	timestamp      json.Number
+	channel        string
+	eventTimestamp json.Number
+}
+
+func Router(r chi.Router) {
+	r.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
+		log.Println(r.Header)
 		var req commonRequest
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 			switch err.(type) {
@@ -47,16 +75,20 @@ func Handler() http.Handler {
 		switch req.Type {
 		case "url_verification":
 			urlVerificationHandler(w, r, req.urlVerificationRequest())
+		case "app_mention":
+			appMentionHandler(w, r, req.appMentionRequest())
 		default:
 			w.WriteHeader(http.StatusNotFound)
 		}
-		return
 	})
-	return mux
 }
 
 func urlVerificationHandler(w http.ResponseWriter, r *http.Request, req *urlVerificationRequest) {
 	if _, err := io.WriteString(w, req.challenge); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
+}
+
+func appMentionHandler(w http.ResponseWriter, r *http.Request, req *appMentionRequest) {
+	log.Println(req.user, req.text)
 }
