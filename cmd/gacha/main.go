5b7140bdfa70687cfccc594f06730329c37a622d
ktr0731 <ktr@syfm.me>
2019-07-15 17:54:03 +0900

    modify entrypoint

--- cmd/gacha/main.go
+++ cmd/gacha/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/GoodCodingFriends/gpay/slack"
+)
 
 func main() {
-	fmt.Println("hello, cloud run")
+	mux := http.NewServeMux()
+	mux.Handle("/slack", slack.Handler())
+	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if err := http.ListenAndServe(addr, nil); err != http.ErrServerClosed {
+		log.Fatalf("failed to launch the server: %s", err)
+	}
 }
